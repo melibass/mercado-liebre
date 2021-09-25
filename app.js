8437a2cf3c20bd2ffd763cb605bdae186e486716
melibass <mbassano@gmail.com>
2021-09-25 20:41:35 -0300

    fin mercado-liebre

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+/* Config public */
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+
+/* Routes */
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/home.html'));
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+})
+
+app.post ('/register', (req,res)=> {
+    console.log ('Ya te registraste campeon')
+})
+
+
+app.listen(3000, () => console.log('Server corriendo en el puerto 3000'));
