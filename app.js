3a97c473bbd3975a13b90221c6acbf2b01e502da
melibass <mbassano@gmail.com>
2021-09-27 21:06:56 -0300

    esta es la buena

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 
 /* Routes */
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/home.html'));
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 })
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 })
 
 app.post ('/register', (req,res)=> {
@@ -23,4 +23,4 @@ app.post ('/register', (req,res)=> {
 })
 
 
-app.listen(3000 || process.env.PORT,() => console.log('Server corriendo en el puerto 3000'));
+app.listen(process.env.PORT|| 3000,() => console.log('Server corriendo en el puerto 3000'));
